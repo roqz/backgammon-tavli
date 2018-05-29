bfab0eea9fa02485cf3f77fefbad44fbdc9d9380
Thomas Ostarek <Thomas.Ostarek@halvotec.de>
2018-05-29 17:02:13 +0200

    Added rule tests for missing implementation

--- src/models/gamerules-backgammon.spec.ts
+++ src/models/gamerules-backgammon.spec.ts
@@ -127,10 +127,88 @@ describe("Gamerules", () => {
         expect(result.points).toBe(3);
     });
     it("should enforce higher roll if only one move can be made", () => {
+        const boardWithState: any = new Board();
+        boardWithState.fields = [
+            // tslint:disable-next-line:max-line-length
+            { "number": 1, "checkers": [{ "color": 1, "idNumber": 6 }, { "color": 1, "idNumber": 7 }, { "color": 1, "idNumber": 5 }, { "color": 1, "idNumber": 0 }, { "color": 1, "idNumber": 1 }, { "color": 1, "idNumber": 2 }, { "color": 1, "idNumber": 3 }, { "color": 1, "idNumber": 4 }, { "color": 1, "idNumber": 9 }, { "color": 1, "idNumber": 10 }, { "color": 1, "idNumber": 11 }, { "color": 1, "idNumber": 12 }, { "color": 1, "idNumber": 13 }, { "color": 1, "idNumber": 14 }] },
+            { "number": 2, "checkers": [] },
+            { "number": 3, "checkers": [] },
+            { "number": 4, "checkers": [] },
+            { "number": 5, "checkers": [] },
+            { "number": 6, "checkers": [] },
+            { "number": 7, "checkers": [] },
+            { "number": 8, "checkers": [] },
+            { "number": 9, "checkers": [] },
+            { "number": 10, "checkers": [] },
+            { "number": 11, "checkers": [] },
+            { "number": 12, "checkers": [] },
+            { "number": 13, "checkers": [{ "color": 0, "idNumber": 14 }] },
+            { "number": 14, "checkers": [{ "color": 0, "idNumber": 12 }, { "color": 0, "idNumber": 13 }] },
+            { "number": 15, "checkers": [] },
+            { "number": 16, "checkers": [{ "color": 0, "idNumber": 0 }, { "color": 0, "idNumber": 1 }] },
+            { "number": 17, "checkers": [{ "color": 0, "idNumber": 2 }, { "color": 0, "idNumber": 11 }] },
+            { "number": 18, "checkers": [{ "color": 0, "idNumber": 3 }, { "color": 0, "idNumber": 4 }] },
+            { "number": 19, "checkers": [{ "color": 0, "idNumber": 5 }, { "color": 0, "idNumber": 6 }] },
+            { "number": 20, "checkers": [{ "color": 0, "idNumber": 7 }, { "color": 0, "idNumber": 8 }] },
+            { "number": 21, "checkers": [{ "color": 0, "idNumber": 9 }, { "color": 0, "idNumber": 10 }] },
+            { "number": 22, "checkers": [] },
+            { "number": 23, "checkers": [] },
+            { "number": 24, "checkers": [{ "color": 1, "idNumber": 8 }] }];
+        boardWithState.bar = { "number": 25, "checkers": [] };
+        // tslint:disable-next-line:max-line-length
+        boardWithState.off = { "number": 0, "checkers": [] };
+        rules = new GamerulesBackgammon(new Board(), p1, p2, diceService, store);
+        rules["_currentPlayer"] = p1;
+        rules["board"] = boardWithState;
 
+        const possibleMoves = rules.getAllPossibleMoves(boardWithState, p1, [1, 2]);
+        expect(possibleMoves.length).toBe(1);
+        const move = possibleMoves[0];
+        expect(move.from).toBe(24);
+        expect(move.to).toBe(22);
+        // es sind nur zwei Züge möglich: 24/22 und 24/23. Daher muss der höhere Wurf gespielt werden.
     });
     it("should enforce move that enables second if otherwise only one is possible", () => {
+        const boardWithState: any = new Board();
+        // tslint:disable-next-line:max-line-length
+        boardWithState.fields = [
+            { "number": 1, "checkers": [{ "color": 0, "idNumber": 12 }, { "color": 0, "idNumber": 13 }] },
+            { "number": 2, "checkers": [{ "color": 1, "idNumber": 5 }] },
+            { "number": 3, "checkers": [] },
+            { "number": 4, "checkers": [{ "color": 1, "idNumber": 6 }, { "color": 1, "idNumber": 7 }] },
+            { "number": 5, "checkers": [] },
+            { "number": 6, "checkers": [{ "color": 1, "idNumber": 8 }] },
+            { "number": 7, "checkers": [] },
+            { "number": 8, "checkers": [] },
+            { "number": 9, "checkers": [] },
+            { "number": 10, "checkers": [] },
+            { "number": 11, "checkers": [] },
+            { "number": 12, "checkers": [] },
+            { "number": 13, "checkers": [{ "color": 0, "idNumber": 14 }] },
+            { "number": 14, "checkers": [] },
+            { "number": 15, "checkers": [] },
+            { "number": 16, "checkers": [{ "color": 0, "idNumber": 0 }, { "color": 0, "idNumber": 1 }] },
+            { "number": 17, "checkers": [{ "color": 0, "idNumber": 2 }, { "color": 0, "idNumber": 11 }] },
+            { "number": 18, "checkers": [{ "color": 0, "idNumber": 3 }, { "color": 0, "idNumber": 4 }] },
+            { "number": 19, "checkers": [{ "color": 0, "idNumber": 5 }, { "color": 0, "idNumber": 6 }] },
+            { "number": 20, "checkers": [{ "color": 0, "idNumber": 7 }, { "color": 0, "idNumber": 8 }] },
+            { "number": 21, "checkers": [{ "color": 0, "idNumber": 9 }, { "color": 0, "idNumber": 10 }] },
+            { "number": 22, "checkers": [] },
+            { "number": 23, "checkers": [] },
+            { "number": 24, "checkers": [] }];
+        boardWithState.bar = { "number": 25, "checkers": [] };
+        // tslint:disable-next-line:max-line-length
+        boardWithState.off = { "number": 0, "checkers": [{ "color": 1, "idNumber": 0 }, { "color": 1, "idNumber": 1 }, { "color": 1, "idNumber": 2 }, { "color": 1, "idNumber": 3 }, { "color": 1, "idNumber": 4 }, { "color": 1, "idNumber": 9 }, { "color": 1, "idNumber": 10 }, { "color": 1, "idNumber": 11 }, { "color": 1, "idNumber": 12 }, { "color": 1, "idNumber": 13 }, { "color": 1, "idNumber": 14 }] };
+        rules = new GamerulesBackgammon(new Board(), p1, p2, diceService, store);
+        rules["_currentPlayer"] = p1;
+        rules["board"] = boardWithState;
 
+        const possibleMoves = rules.getAllPossibleMoves(boardWithState, p1, [6, 3]);
+        expect(possibleMoves.length).toBe(1);
+        const move = possibleMoves[0];
+        expect(move.from).toBe(6);
+        expect(move.to).toBe(3);
+        // damit beide züge gespielt werden können muss zuerst 6/3 gespielt werden, damit danach mit der 6 noch 4/0 geht
     });
     it("should calculate correct possible moves 1", () => {
         const boardWithState: any = new Board();
