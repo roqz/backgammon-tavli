1d825163269a6b05de34f18839c2ebd03486646a
Roqz <thomas.ostarek@googlemail.com>
2018-05-31 15:31:59 +0200

    restructured project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
 
 import { AppComponent } from "./app.component";
-import { DiceComponent } from "./components/dice.component";
+import { DiceComponent } from "./dice/dice.component";
 import { StoreModule } from "@ngrx/store";
 import { reducers, metaReducers } from "./reducers";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "../environments/environment";
 import { EffectsModule } from "@ngrx/effects";
 import { AppEffects } from "./app.effects";
-import { GameHistoryComponent } from "./components/gamehistory.component";
+import { GameHistoryComponent } from "./gamehistory/gamehistory.component";
+import { GameresultComponent } from "./gameresult/gameresult.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DiceComponent,
-    GameHistoryComponent
+    GameHistoryComponent,
+    GameresultComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gameresult/gameresult.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gameresult',
+  templateUrl: './gameresult.component.html',
+  styleUrls: ['./gameresult.component.css']
+})
+export class GameresultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
