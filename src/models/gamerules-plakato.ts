e8b837653c0930a9a951c1d717e677c8ca3105ac
Thomas Ostarek <Thomas.Ostarek@halvotec.de>
2018-06-08 16:15:37 +0200

    Anmerkungend

--- src/models/gamerules-plakato.ts
+++ src/models/gamerules-plakato.ts
@@ -1,12 +1,12 @@
-import { GameRulesBase } from "./gamerulesbase";
-import { Board } from "./board";
-import { Player } from "./player";
-import { Move } from "./move";
-import { GameResult } from "./gameresult";
-import { DiceService } from "../services/dice.service";
-import { GameMode } from "./gamemode";
 import { Store } from "@ngrx/store";
 import { State } from "../app/reducers";
+import { DiceService } from "../services/dice.service";
+import { Board } from "./board";
+import { GameMode } from "./gamemode";
+import { GameResult } from "./gameresult";
+import { GameRulesBase } from "./gamerulesbase";
+import { Move } from "./move";
+import { Player } from "./player";
 
 export class GamerulesPlakato extends GameRulesBase {
     constructor(board: Board, player1: Player, player2: Player, dice: DiceService, store: Store<State>) {
@@ -14,22 +14,30 @@ export class GamerulesPlakato extends GameRulesBase {
         this._doublerCubeEnabled = false;
     }
     public start() {
+        // wie tavli, andere board init positionen
         throw new Error("Method not implemented.");
     }
     public getAllPossibleMoves(board: Board, player: Player, diceRolls: number[]): Move[] {
+        // wie tavli, unterschied: blockierte steine können nicht bewegt werden
         throw new Error("Method not implemented.");
     }
     public makeMove(move: Move, player: Player) {
+        // unterschied: kein werfen sondern blockieren
         throw new Error("Method not implemented.");
     }
     public revertLastMove() {
+        // wie tavli
         throw new Error("Method not implemented.");
     }
     public finishTurn(player: Player) {
+        // wie tavli
         throw new Error("Method not implemented.");
     }
     public getResult(): GameResult {
+        // wie tavli
         throw new Error("Method not implemented.");
     }
+
+    // is game over prüfung ergänzen: wenn der 1 steine blockiert ist und der gegner keinen stein mehr auf der 1 hat immediate loss
 
 }
--- src/models/gamerules-portes.ts
+++ src/models/gamerules-portes.ts
@@ -1,12 +1,12 @@
-import { GameRulesBase } from "./gamerulesbase";
-import { Board } from "./board";
-import { Player } from "./player";
-import { Move } from "./move";
-import { GameResult } from "./gameresult";
-import { DiceService } from "../services/dice.service";
 import { Store } from "@ngrx/store";
 import { State } from "../app/reducers";
+import { DiceService } from "../services/dice.service";
+import { Board } from "./board";
 import { GameMode } from "./gamemode";
+import { GameResult } from "./gameresult";
+import { GameRulesBase } from "./gamerulesbase";
+import { Move } from "./move";
+import { Player } from "./player";
 
 export class GamerulesPortes extends GameRulesBase {
     constructor(board: Board, player1: Player, player2: Player, dice: DiceService, store: Store<State>) {
@@ -14,21 +14,28 @@ export class GamerulesPortes extends GameRulesBase {
         this._doublerCubeEnabled = false;
     }
     public start() {
+        // andere start positionen
         throw new Error("Method not implemented.");
     }
     public getAllPossibleMoves(board: Board, player: Player, diceRolls: number[]): Move[] {
+        // es kann immer nur ein stein auf einem feld sein
+        // beide gehen in gleiche richtung
         throw new Error("Method not implemented.");
     }
     public makeMove(move: Move, player: Player) {
+        // kein werfen, nur ein stein pro feld
         throw new Error("Method not implemented.");
     }
     public revertLastMove() {
+        // wie tavli
         throw new Error("Method not implemented.");
     }
     public finishTurn(player: Player) {
+        // wie tavli
         throw new Error("Method not implemented.");
     }
     public getResult(): GameResult {
+        // wie tavli
         throw new Error("Method not implemented.");
     }
 
